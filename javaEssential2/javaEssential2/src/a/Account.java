7d729660a2ae7f1f86f3fe984d7ee14e8c20757c
telpvn <telpvn@mail.ru>
2024-05-12 14:32:30 +0400

    Account 2

--- javaEssential2/javaEssential2/src/a/Account.java
+++ javaEssential2/javaEssential2/src/a/Account.java
@@ -1,17 +1,11 @@
 package a;
 
 public class Account {
-    double UAN;
-    String name;
-    static double courseUSDtoUAN;
+    static int b = 30;
 
     public static void main(String[] args) {
-        Account a1 = new Account();
-        a1.name = "Robert";
-        a1.UAN = 2000.05;
-        Account.courseUSDtoUAN = 26.1;
-        Account a2 = new Account();
-        a2.name = "Edward";
-        a2.UAN = 1800;
+        int a = 10;
+        System.out.println(a);
+        System.out.println(Account.b);
     }
 }
